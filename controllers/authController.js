921aae9e28bcd943df6c73b361ce9796268da762
purna25 <purnanandahari@gmail.com>
2021-09-11 17:24:39 +0530

    ADD: login in route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,54 @@
 const User = require("../models/userModel")
 
+const bcrypt = require("bcryptjs")
+const { use } = require("../routes/userRoutes")
+
+
 exports.signUp = async (req, res) => {
+    const {username, password} = req.body
+    const hashPassword = await bcrypt.hash(password, 12)
     try {
-        const newUser = await User.create(req.body)
+        const newUser = await User.create({
+            username,
+            password: hashPassword
+        })
         res.status(201).json({
             status: "success",
             data: {
                 user: newUser
             }
         })
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({
+            status: "fail"
+        })
+        
+    }
+}
+
+exports.login = async (req, res) => {
+    const {username, password} = req.body
+    try {
+        const user = await User.findOne({username})
+        if (!user) {
+            return res.status(404).json({
+                status: "fail",
+                message: "user not found"
+            })
+        }
+
+        const isCorrect = await bcrypt.compare(password, user.password)
+        if (isCorrect) {
+            res.status(200).json({
+                status: "success"
+            })
+        } else {
+            res.status(400).json({
+                status: "fail",
+                message: "Incorrect username or password"
+            })
+        }
     } catch (e) {
         console.log(e)
         res.status(400).json({
