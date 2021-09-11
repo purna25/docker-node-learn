f92974b4a6fcf41c4e5586a0635a2bc3b4d86570
purna25 <purnanandahari@gmail.com>
2021-09-11 15:42:55 +0530

    ADD: connection to mongoDB from node

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useFindAndModify: false
+})
+.then(() => console.log("Connected to DB"))
+.catch((e)=>console.log(e))
+
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("<h2> Hi There!!!!!!!!</h2>");
+    res.send("<h2> Hi There!!!</h2>");
 });
 
 const port = process.env.PORT || 3000
