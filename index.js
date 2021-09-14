a3483643798de622449ceaa8c6fe4e45d25a2e5b
purna25 <purnanandahari@gmail.com>
2021-09-14 21:53:21 +0530

    ADD: added cors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const session = require("express-session");
 const redis = require("redis");
-
+const cors = require("cors");
 
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_URL, REDIS_PORT, SESSION_SECRET } = require("./config/config");
 
@@ -38,6 +38,8 @@ const app = express();
 
 
 app.enable("trust proxy")
+app.use(cors({}))
+
 app.use(session({
     store: new RedisStore({client: redisClient}),
     secret: SESSION_SECRET,
